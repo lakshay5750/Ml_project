54b512c6a6c717d6b8b191545af485b874f46d79
lakshay5750 <lakshyavarshney62@gmail.com>
2025-06-07 13:27:42 +0530

    Data Transformation Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,8 @@ from src.exception import CustomException
 from src.logger import logging
 from sklearn.model_selection import train_test_split
 import pandas as pd
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTranformationConfig
 from dataclasses import dataclass
 @dataclass
 class DataIngestionConfig:
@@ -29,11 +31,18 @@ class DataIngestion:
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
             logging.info("ingestion of the data is completed")
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+            )
         except Exception as e:
             raise CustomException(e,sys)
 
 
 if __name__=='__main__':
     obj= DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
